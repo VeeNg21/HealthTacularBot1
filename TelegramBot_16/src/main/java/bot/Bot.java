0b04a1ac6ccd7d6870f3e63f5fd6cf3ee429dffc
Vee21 <55953389+VeeNg21@users.noreply.github.com>
2021-11-16 18:10:35 +0500

    Update Bot.java

--- TelegramBot_16/src/main/java/bot/Bot.java
+++ TelegramBot_16/src/main/java/bot/Bot.java
@@ -3,20 +3,22 @@ package bot;
 import bot.Commands.PlayCommand;
 import bot.Commands.StartCommand;
 import bot.Game.MainGame;
-import bot.Game.DefaultPlayer;
 import org.telegram.telegrambots.extensions.bots.commandbot.TelegramLongPollingCommandBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.sql.SQLException;
 
 public class Bot extends TelegramLongPollingCommandBot {
 
     private final String botName;
     private final String botToken;
 
+    DbHandler dbHandler = DbHandler.getInstance();
     MainGame mainGame = new MainGame();
 
-    public Bot(String botName, String botToken){
+    public Bot(String botName, String botToken) throws SQLException {
         this.botName = botName;
         this.botToken = botToken;
 
@@ -35,10 +37,19 @@ public class Bot extends TelegramLongPollingCommandBot {
         return botToken;
     }
 
+
+    /**
+     * Ответ на запрос, не являющийся командой
+     */
     @Override
     public void processNonCommandUpdate(Update update) {
 
-        String msg = mainGame.inGame(update);
+        String msg = null;
+        try {
+            msg = mainGame.inGame(update);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         if(!msg.equals("")) sendAnswer(update.getMessage().getChatId(), msg);
     }
 
